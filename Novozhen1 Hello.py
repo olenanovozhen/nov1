ce3aea427711d07d00e8eb78d72931911523dbd8
olenanovozhen <lennovozhen@ukr.net>
2023-03-14 17:08:15 +0300

    update all file

---
+++ Novozhen1 Hello.py
@@ -0,0 +1,36 @@
+print("Hello")
+s1 = "I am"
+s2 = "from Ukraine"
+print (s1 + " " + s2)
+a = input()
+print(a)
+
+
+input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
--- Novozhen4 If.py
+++ Novozhen4 If.py
@@ -10,6 +10,8 @@ else:
     print("England")
 
 
+ input()
+
 
 
 
